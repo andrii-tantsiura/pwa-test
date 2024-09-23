47a95edc8e678710bef7b5af05b2c2abd510a8b4
andrii.t <andrii.t@halo-lab.team>
2024-09-23 10:12:16 +0300

    Added some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const VIBRATION_PATTERN = [
+  100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
+];
 
 function App() {
+  const [coords, setCoords] = useState<GeolocationCoordinates | undefined>();
+  const [locationSearchUrl, setLocationSearchUrl] = useState<
+    string | undefined
+  >();
+
+  const fetchCurrentPosition = useCallback(async () => {
+    const permission = await navigator.permissions.query({
+      name: "geolocation",
+    });
+
+    if (permission.state === "granted") {
+      navigator.geolocation.getCurrentPosition(({ coords }) =>
+        setCoords(coords)
+      );
+    } else if (permission.state === "prompt") {
+      // showButtonToEnableLocalNews();
+    } else {
+      // alert("Location permissions denied");
+    }
+  }, []);
+
+  const handleShare = () => {
+    const sharedLocation = {
+      text: "Share my location",
+      title: "Share",
+      url: locationSearchUrl,
+    };
+
+    if (navigator.canShare(sharedLocation)) {
+      navigator.share(sharedLocation);
+    } else {
+      alert("Cannot share location");
+    }
+  };
+
+  const handleCopy = () => {
+    if (locationSearchUrl) {
+      navigator.clipboard.writeText(locationSearchUrl);
+    }
+  };
+
+  const handleChangeBadgeNumber = () => (navigator as any)?.setAppBadge(9);
+
+  const handleVibrate = () => navigator.vibrate(VIBRATION_PATTERN);
+
+  useEffect(() => {
+    setLocationSearchUrl(
+      `https://www.google.com/search?q=${coords?.latitude}+${coords?.longitude}`
+    );
+  }, [coords?.latitude, coords?.longitude]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={fetchCurrentPosition}>Get my location</button>
+
+        <button onClick={handleChangeBadgeNumber}>Set app badge 9</button>
+
+        <button onClick={handleVibrate}>Vibrate SOS</button>
+
+        {coords && (
+          <>
+            <p>
+              My location: {coords.latitude}, {coords.longitude}
+            </p>
+
+            <a
+              className="App-link"
+              href={locationSearchUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Open Google Maps
+            </a>
+
+            <button onClick={handleShare}>Share my location</button>
+
+            <button onClick={handleCopy}>Copy</button>
+          </>
+        )}
       </header>
     </div>
   );
