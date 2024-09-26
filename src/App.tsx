82781794046ac1ee02f01e1715e1fbbb96aaadcc
andrii.t <andrii.t@halo-lab.team>
2024-09-26 09:42:19 +0300

    Added page 404

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { About } from "./Pages/About";
 import { Home } from "./Pages/Home";
 import { Layout } from "./Pages/Layout";
+import { Page404 } from "./Pages/Page404";
 import { Users } from "./Pages/Users";
 
 import "./App.css";
@@ -11,10 +12,11 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route element={<Layout />}>
           <Route index path="home" element={<Home />} />
           <Route path="users" element={<Users />} />
           <Route path="about" element={<About />} />
+          <Route path="*" element={<Page404 />} />
         </Route>
       </Routes>
     </div>
--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,5 +1,5 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
